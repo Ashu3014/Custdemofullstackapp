93bc190f8de451c1bbbbef9532d559a53877e1d2
Ashu3014 <126841142+Ashu3014@users.noreply.github.com>
2023-04-16 20:09:48 -0700

    First Commite-fullstackapp

---
+++ insertdata/insertdata.component.ts
@@ -0,0 +1,19 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insertdata',
+  templateUrl: './insertdata.component.html',
+  styleUrls: ['./insertdata.component.css']
+})
+export class InsertdataComponent {
+
+  constructor() { }
+
+  ngOnInit() : void{ }
+
+  insertcustdata(insertform:any)
+  {
+
+  }
+
+}
